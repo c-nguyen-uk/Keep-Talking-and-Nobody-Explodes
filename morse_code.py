e779233a8e79aabcc028f4a67a8d054bb039db4b
Christopher Nguyen <61757292+c-nguyen-uk@users.noreply.github.com>
2020-03-07 13:10:21 +0000

    Add files via upload

---
+++ morse_code.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+This programme solves the Morse Code module.
+
+It is important that we allow for input forgiveness since the defuser is
+likely to misread the morse code. Hence we only reqiure that the initial part
+of the morse code needs to be fully correct. The programme will then suggest
+possible correct frequencies. If enough lives are available, then all of them
+can be attempted. Otherwise, more correct morse code will need to be appended.
+"""
+
+# Creating dictionaries of the possible morse code, words and frequencies.
+
+morse_to_words = {".........-...-..":"shell", ".....-.-...-.....":"halls",
+                "....-....-.-.-.-":"slick", "-.-...-.-.-.-":"trick",
+                "-...----..-....":"boxes", ".-....--.-...":"leaks",
+                "...-.-.----....":"strobe", "-........-.-.---":"bistro",
+                "..-..-....-.-.-.-":"flick","-...------......":"bombs",
+                "-....-...--.-":"break", "-....-...-.-.-.-":"brick",
+                "...-..--.-":"steak", "...-..-.--.":"sting",
+                "...-.-.-.----.-.":"vector", "-.....--...":"beats"}
+words_to_freqs = {"shell":"3.505", "halls":"3.515", "slick":"3.522",
+                 "trick":"3.532", "boxes":"3.535", "leaks":"3.542",
+                 "strobe":"3.545", "bistro":"3.552", "flick":"3.555",
+                 "bombs":"3.565", "break":"3.572", "brick":"3.575",
+                 "steak":"3.582", "sting":"3.592", "vector":"3.595",
+                 "beats":"3.600"}
+
+# This function returns possible frequencies corresponding to the morse code.
+
+def morse_code(morse):
+    freqs_list = []
+    for i in morse_to_words:
+        if morse in i:
+            word = morse_to_words[i]
+            freq = words_to_freqs[word]
+            freqs_list.append(freq)
+    if freqs_list == []:
+       return "The morse code is wrong." 
+    return "The frequency could be: {f} MHz.".format(f=", ".join(freqs_list))
+
+# We now ask for morse code.
+
+while True:
+    morse = str(input("Dots and dashes without spaces:"))
+    input(morse_code(morse) + "\n \nPress Enter to retry.")
---
+++ password_cracker.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+This programme solves the Password Cracker module.
+
+We do not discriminate between letter capitalisation or input - as long as
+there is a comma between them. There is also some input forgiveness - if no
+password can be found, then retrying is possible.
+"""
+
+# We create a list of all possible passwords.
+
+passwords = ["ABOUT", "AFTER", "AGAIN", "BELOW", "COULD", "EVERY", "FIRST",
+             "FOUND", "GREAT", "HOUSE", "LARGE", "LEARN", "NEVER", "OTHER",
+             "PLACE", "PLANT", "POINT", "RIGHT", "SMALL", "SOUND", "SPELL",
+             "STILL", "STUDY", "THEIR", "THERE", "THESE", "THING", "THINK",
+             "THREE", "WATER", "WHERE", "WHICH", "WORLD", "WOULD", "WRITE"]
+
+# This simple function checks for a password from any possible combinations of
+# letters. Despite being O(n^5), it runs quickly since n = 6 in this module.
+# Note that in this module answers are unique.
+
+def password_cracker(L1, L2, L3, L4, L5):
+    for i1 in L1:
+        for i2 in L2:
+            for i3 in L3:
+                for i4 in L4:
+                    for i5 in L5:
+                        if i1 + i2 + i3 + i4 + i5 in passwords:
+                            return ("The password is: "
+                                    + i1 + i2 + i3 + i4 + i5)
+
+# We now ask for the sets of letters.
+ 
+while True:
+    L1 = list(input("First letters, separated by a comma: ").upper())
+    L2 = list(input("Second letters, separated by a comma: ").upper())
+    L3 = list(input("Third letters, separated by a comma: ").upper())
+    L4 = list(input("Fourth letters, separated by a comma: ").upper())
+    L5 = list(input("Fifth letters, separated by a comma: ").upper())
+    while not (password_cracker(L1, L2, L3, L4, L5) in passwords):
+        input("No password exists. Press enter to try again.")
+        L1 = list(input("First letters, separated by a comma: ").upper())
+        L2 = list(input("Second letters, separated by a comma: ").upper())
+        L3 = list(input("Third letters, separated by a comma: ").upper())
+        L4 = list(input("Fourth letters, separated by a comma: ").upper())
+        L5 = list(input("Fifth letters, separated by a comma: ").upper())
+    input(password_cracker(L1, L2, L3, L4, L5) + "\n \nPress Enter to retry.")
